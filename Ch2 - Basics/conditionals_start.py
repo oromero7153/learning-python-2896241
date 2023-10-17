d0848bda73f6408e1f64d9511ee78297c1567ce7
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-10-17 13:20:54 -0700

    worked on some linkedin learning

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -6,9 +6,17 @@
 
 
 def main():
-    x, y = 10, 100
+    x, y = 1000, 100
 
     # conditional flow uses if, elif, else
+    if x < y:
+        result = "x is less than y"
+    elif x == y:
+        result = "x is the same as y"
+    else:
+        result = "x is greater than y" 
+
+    print(result)
 
     # conditional statements let you use "a if C else b"
 
